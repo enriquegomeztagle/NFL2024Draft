7a8daed9c7ca6353c0476ae6d249713f242e27dc
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-09-10 17:12:25 -0600

    refactor: clean training_loss_analysis.py

--- training_loss_analysis.py
+++ training_loss_analysis.py
@@ -1,11 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.impute import KNNImputer
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.model_selection import cross_val_score
-import numpy as np
 import matplotlib.pyplot as plt
 
 # Cargar los datos
